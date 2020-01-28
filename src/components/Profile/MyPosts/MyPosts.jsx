59420bf128af53465be2ba13508f6b14e7f1a802
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-01-28 22:17:03 +0300

    action creator и action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from './../../../redux/state';
 
 const MyPosts = (props) => {
 
@@ -8,15 +9,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        //props.addPost();
-        props.dispatch({ type: 'ADD-POST' });
+    let addPost = () => {        
+        props.dispatch(addPostActionCreator());
     }
 
     let onPostChange = () => {
-        let text = newPostElement.current.value;
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };        
-        props.dispatch(action)
+        let text = newPostElement.current.value;        
+        props.dispatch(updateNewPostTextActionCreator(text))
     }
 
     return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -29,9 +32,9 @@ let store = {
     subscribe(observer) {
         this._callSubscriber = observer;//observer - наблюдатель
     },
-    
+
     dispatch(action) { //{type: 'ADD-POST'}
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 3,
                 message: this._state.profilePage.newPostText,
@@ -40,12 +43,15 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
 }
+
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 
 export default store;
 window.store = store;
