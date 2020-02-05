6de6a76bd0792cc05ce983facadc96136a180e2e
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-02-05 20:06:36 +0300

    60. withRouter, props.match.params. Используем withRouter для обертывания компоненты ProfileContainer, тем самым получаем доступ из этой компоненты к параметрам URL

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import Profile from './Profile';
 import { setUserProfile } from '../../redux/profile-reducer';
 
 class ProfileContainer extends React.Component {
-
+    
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+
+        let userId = this.props.match.params.userId;
+        if (!userId) {
+            userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(responce => {               
                 this.props.setUserProfile(responce.data);
             });
@@ -28,4 +34,6 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 });
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './ProfileInfo.module.css'
+import styles from './ProfileInfo.module.css'
 import Preloader from '../../common/preloader/Preloader';
 
 const ProfileInfo = (props) => {
@@ -12,9 +12,9 @@ const ProfileInfo = (props) => {
             <div>
                 <img src="https://www.softrew.ru/wp-content/cache/thumb/75/fb4c03e3eee2d75_810x260.png" alt='' />
             </div>
-            <div className={s.descriptionBlock}>
+            <div className={styles.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=''/>
-                ava + description
+                
             </div>
         </div>
     )
