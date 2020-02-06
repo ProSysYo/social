bee7b3d0569e303f9ea76b267d4d10da670d6eaa
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-02-06 21:51:49 +0300

    61. coocie, login в теории, auth_me. Добавление авторизации

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route } from 'react-router-dom';
@@ -10,7 +10,7 @@ import ProfileContainer from './components/Profile/ProfileContainer';
 const App = (props) => {
     return (
         <div className="app-wrapper">
-            <Header />
+            <HeaderContainer />
             <Navbar />
             <div className='app-wrapper-content'>
                 <Route path='/dialogs' render={() => <DialogsContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
         <img src='https://www.logaster.ru/static/v3/img/products/logo.png' alt=''/>
+
+        <div className= {s.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to="/login">Login</NavLink>}
+            
+        </div>
     </header>
   )
 }
