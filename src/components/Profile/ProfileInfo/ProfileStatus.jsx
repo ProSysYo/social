12696235ec13cb26d70c5498d90470228236c5e8
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-02-10 20:29:14 +0300

    71. setState, local state Часть 2

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 
 class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    }
+
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        })
+
+    }
+
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        })
+
+    }
+
     render() {
         return (
             <div>
-                <div>
-                    <span>{this.props.status}</span>
-                </div>
-                <div>
-                    <input value={this.props.status} />
-                </div>
+                {!this.state.editMode &&
+                    <div>
+                        <span
+                            onDoubleClick={this.activateEditMode.bind(this)}
+                        >{this.props.status}</span>
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input
+                            value={this.props.status}
+                            onBlur={this.deactivateEditMode.bind(this)}
+                            autoFocus={true}
+                        />
+                    </div>
+                }
+
             </div>
         )
     }
