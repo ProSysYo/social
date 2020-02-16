c4655c0bb1ace4f0104a496276928a506f1a62cd
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-02-16 22:14:55 +0300

    80. инициализация приложения. пользователь теперь инициализируется в App.js

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -29,7 +29,7 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
 
 export const getAuthUserData = () => {
     return (dispatch) => {
-        authAPI.me()        
+        return authAPI.me()        
             .then(data => {   
                 if (data.resultCode ===0) {
                     let {id, email, login} = data.data;
