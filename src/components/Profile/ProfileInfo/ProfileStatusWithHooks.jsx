f2fa2e08740616ff8e89401c4f6e9549a9498891
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-02-19 19:50:40 +0300

    85. useEffect. использовал в компоненте ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const ProfileStatusWithHooks = (props) => {
-    
+
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
